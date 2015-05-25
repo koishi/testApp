175541db5a26af4e58204d44e646e66a41e75d17
Koichiro Oishi <bs@v7.com>
2015-05-25 20:34:44 +0900

    Objective-C学習

--- testApp/ViewController.m
+++ testApp/ViewController.m
@@ -17,6 +17,43 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  
+  NSString *string;
+  string = @"fdfdfafa";
+
+  NSInteger integer;
+  integer = 10;
+
+
+  NSLog(@"%@",string);
+  
+  NSLog(@"%ld",(long)integer);
+
+  NSString *const constText = @"FILE_NAME";
+  NSLog(@"%@",constText);
+
+  if (integer == 10) {
+    NSLog(@"10でした");
+  }
+
+  // コメント
+  if ([string isEqualToString:@"あ"]) {
+    NSLog(@"一致しました");
+  } else {
+    NSLog(@"一致しませんでした");
+  }
+
+  NSArray *array = @[@"一郎",@"二郎"];
+  NSLog(@"%@",array);
+
+  NSMutableArray *mArray = [[NSMutableArray alloc]init];
+  [mArray addObject:@"teatafa"];
+  [mArray addObject:@"aaaaa"];
+  NSLog(@"%lu",(unsigned long)[mArray count]);
+  NSLog(@"%@",mArray);
+  [mArray removeObjectAtIndex:1];
+  NSLog(@"%@",mArray);
+
 }
 
 - (void)didReceiveMemoryWarning {
